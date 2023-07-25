22cfbb7c567792008fa95445b4fe63a415ab2645
xavierrice <xavier.rice@gmail.com>
2023-07-25 12:40:42 -0400

    added contact list

--- src/Components/Contacts.jsx
+++ src/Components/Contacts.jsx
@@ -1,9 +1,11 @@
+import ContactList from "./ContactUserCard"
+
 function Contacts(){
  return(
   <>
   <div className="Contacts">
   <h2>Contacts</h2>
-   <ul>Contacts User Card</ul>
+   <ul><ContactList/></ul>
    </div>
   </>
  )
