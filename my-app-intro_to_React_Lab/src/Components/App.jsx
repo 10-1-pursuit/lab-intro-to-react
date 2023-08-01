09a2caa3e6e5d3b097a5760fd49c448409026ad1
Rose <i.a.rose.bk@gmail.com>
2023-08-01 18:48:48 -0400

    Connected jsx components

---
+++ my-app-intro_to_React_Lab/src/Components/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./index.css";
+import '../App.css'
+
+
+import NavBar from './NavBar'
+import Contacts from './Contacts.jsx'
+import Posts from './Posts'
+import UserProfile from './UserProfile'
+// import App from '../Components'
+
+
+function App() {
+  return (
+    <div>
+     <p>Hello, world!</p>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app-intro_to_React_Lab/src/Components/Posts.jsx
@@ -0,0 +1,9 @@
+const Posts =()=>{
+
+    return (
+
+<h1>HEY</h1>   
+ )
+
+}
+export default Posts
