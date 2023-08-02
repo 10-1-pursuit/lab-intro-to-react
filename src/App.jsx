100cc549587f403d9e7c65e32b87c71cd3153090
yulondawyatt@gmail.com <yulondajoywyatt@Yulondas-MacBook-Air.local>
2023-08-02 11:09:09 -0400

    First commit. Rendered list from NavBar to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./index.css";
+import NavBar from '/src/Components/NavBar.jsx'
 
 function App() {
-  return <p>Hello, world!</p>;
+  return (
+  <>
+  <p>Hello, world!</p>
+
+  <div className="Container">
+    <NavBar />
+  </div>
+  </>
+  
+  );
+  
+
 }
 
 export default App;
