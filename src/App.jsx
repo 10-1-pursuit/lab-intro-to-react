fedebf136d1579c5ca82264e36017d2fbd7ee0f7
Rubby <rubenslombard@pursuit.org>
2023-08-02 13:05:46 -0400

    homework done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./index.css";
 
+import UserProfile from "./Components/UserProfile";
+import NavBar from "./Components/NavBar";
+import Posts from "./Components/Posts";
+import Contacts from "./Components/Contacts";
+
 function App() {
-  return <p>Hello, world!</p>;
+  return (
+    <>
+      <NavBar />
+      <UserProfile />
+      <Posts />
+      <Contacts />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/ContactUserCard.jsx
@@ -0,0 +1,20 @@
+function ContactUser() {
+  return (
+    <>
+      <div className="user">
+        <img
+          src="./src/profile.png"
+          alt="UserProfile"
+          width="110"
+          height="120"
+        />
+      </div>
+      <div className="userContact">
+        <h4>Jordan Walke</h4>
+        <h5>React Creator</h5>
+        <h6>Lorem Imsem</h6>
+      </div>
+    </>
+  );
+}
+export default ContactUser;
