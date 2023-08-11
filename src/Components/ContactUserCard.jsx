b8dea1bc902cfb70ac6c33286dbf083d6a321771
Lamb144 <alvinjohnson144@gmail.com>
2023-08-11 15:54:57 -0400

    intro react Complete Lab

--- src/Components/ContactUserCard.jsx
+++ src/Components/ContactUserCard.jsx
@@ -1,10 +1,11 @@
-// import Contacts from "./Contacts";
+import Contacts from "./Contacts";
 
 function ContactUserCard() {
     return (
         <div className="contactusercard">
             <div className="contactusercardheader">
                 <h3>Contacts</h3>
+                <Contacts />
             </div>
         </div>
     )
---
+++ src/Components/Contacts.jsx
@@ -0,0 +1,50 @@
+
+function Contacts() {
+    return (
+        <>
+            <div className="contacts1">
+                <img className="moveimagecontacts" src="src/profile.png" alt="image-silhourtte" />
+                <div className="movethisboxcontacts">
+                    <h3>New User</h3>
+                    <p><b>React Creator</b></p>
+                    <p>Lorem Ipsem</p>
+                </div>
+            </div>
+            <div className="contacts2">
+                <img className="moveimagecontacts" src="src/profile.png" alt="image-silhourtte" />
+                <div className="movethisboxcontacts">
+                    <h3>New User</h3>
+                    <p><b>React Creator</b></p>
+                    <p>Lorem Ipsem</p>
+                </div>
+            </div>
+            <div className="contacts3">
+                <img className="moveimagecontacts" src="src/profile.png" alt="image-silhourtte" />
+                <div className="movethisboxcontacts">
+                    <h3>New User</h3>
+                    <p><b>React Creator</b></p>
+                    <p>Lorem Ipsem</p>
+                </div>
+            </div>
+            <div className="contacts4">
+                <img className="moveimagecontacts" src="src/profile.png" alt="image-silhourtte" />
+                <div className="movethisboxcontacts">
+                    <h3>New User</h3>
+                    <p><b>React Creator</b></p>
+                    <p>Lorem Ipsem</p>
+                </div>
+            </div>
+            <div className="contacts5">
+                <img className="moveimagecontacts" src="src/profile.png" alt="image-silhourtte" />
+                <div className="movethisboxcontacts">
+                    <h3>New User</h3>
+                    <p><b>React Creator</b></p>
+                    <p>Lorem Ipsem</p>
+                </div>
+            </div>
+        </>
+
+    )
+}
+
+export default Contacts;
