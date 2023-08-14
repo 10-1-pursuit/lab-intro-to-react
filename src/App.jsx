04063fdcd97106e03a689fb4f9cbbfffda7158f6
Kenneth-Parker <kennethkavon@gmail.com>
2023-08-14 14:03:47 -0400

    imported components links and added to return aka jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./index.css";
+import Contacts from "./Components/Contacts";
+import ContactUserCard from "./Components/ContactUserCard";
+import NavBar from "./Components/NavBar";
+import Post from "./Components/Post";
+import Posts from "./Components/Posts";
+import UserProfile from "./Components/UserProfile";
 
 function App() {
-  return <p>Yes Hello, world!</p>;
+  return <>
+  <p>Yes Hello, world!</p>
+
+  <Contacts/>
+  <ContactUserCard/>
+  <NavBar/>
+  <Post/>
+  <Posts/>
+  <UserProfile/>
+
+  </>
 }
 
 export default App;
