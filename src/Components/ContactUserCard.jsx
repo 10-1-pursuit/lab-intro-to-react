0b4aa42da0268b62af86966f29e9b53881f6ab0d
Kenneth-Parker <kennethkavon@gmail.com>
2023-08-14 13:54:07 -0400

    added basic functionality to componets with export

---
+++ src/Components/ContactUserCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ContactUserCard() {
+    return (
+        <div>
+            <h2>ContactUserCard</h2>
+            { }
+        </div>
+    );
+}
+
+export default ContactUserCard;
