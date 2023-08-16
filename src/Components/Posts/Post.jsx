a504a1c16e17dd5835ab22d35e4d2a4168d54188
Kenneth-Parker <kennethkavon@gmail.com>
2023-08-16 00:32:08 -0400

    drk mode update more or less

--- src/Components/Posts/Post.jsx
+++ src/Components/Posts/Post.jsx
@@ -5,6 +5,7 @@ function Post() {
     return (
         <div className='post'>
             <h2>Post</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio doloribus consequatur fugit neque recusandae voluptas itaque, nobis aut incidunt similique delectus harum!</p>
             { }
         </div>
     );
